1b8b0cf436ffddacf8245d8c2b8439c9739ef329
Jørgen Reimers <jorgenreim@gmail.com>
2021-10-26 19:08:57 +0200

    die

--- jorgen/1.py
+++ jorgen/1.py
@@ -1,3 +1,3 @@
 import sys
 c="aeiouyAEIOUY "
-sys.stdout.write("".join([l+'o' if l not in c else l for l in sys.stdin.read()[:-1]]))
+sys.stdout.write("".join([l+'o'+l.lower() if l not in c else l for l in sys.stdin.read()[:-1]]))
